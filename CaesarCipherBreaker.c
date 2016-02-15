fd8d9fa2e24150592ada1289c12208d5f3003918
Charles <charles@revel.in>
2016-02-15 13:19:51 -0500

    Initial

---
+++ CaesarCipherBreaker.c
@@ -0,0 +1,27 @@
+#import <stdio.h>
+
+#define TOTAL 26
+
+void readFreq (float given[], char fname[]) {
+	
+}
+
+void calcFreq (float found[], char fname[]) {
+	
+}
+
+char rotate (char ch, int num) {
+	
+}
+
+int findKey (float given[], float found[]) {
+	
+}
+
+void decrypt (int, char fname[]) {
+	
+}
+
+int main () {
+	
+}
