7d47dc160bc025ba811422eccc46df3dafacba50
Charles <charles@revel.in>
2016-02-15 16:21:46 -0500

    calcFreq

--- CaesarCipherBreaker.c
+++ CaesarCipherBreaker.c
@@ -1,34 +1,53 @@
 #import <stdio.h>
 #import <stdlib.h>
 
-#define TOTAL 26
+#define ALPH 26
+#define SIZE 1024
 
 char letterFreqFile[] = "LetFreq.txt";
-float letFreq[26];
+char readFile[] = "InputText.txt";
+float letFreq[ALPH*2], inputFreq[ALPH*2];
 
 void readFreq (float given[], char fname[]) {
-	FILE *ifp;
-	char *mode = "r";
-	
-	ifp = fopen(fname, mode);
+	FILE *ifp = fopen(fname, "r");
 	
 	if (ifp == NULL) {
 		fprintf(stderr, "Can't open input file LetFreq.txt\n");
 		exit(1);
 	}
 	
-	char letter[1];
+	char *letter;
 	float freq;
 	int i=0;
-	while (fscanf(ifp, "%s %f", letter, &freq) != EOF) {
+	while (fscanf(ifp, "%c %f\n", letter, &freq) != EOF) {
+		letFreq[ALPH+i] = freq;
 		letFreq[i++] = freq;
 	}
 	
-	fclose(ifp);
 }
 
 void calcFreq (float found[], char fname[]) {
+	FILE *ifp = fopen(fname, "r");
 	
+	if (ifp == NULL) {
+		fprintf(stderr, "Can't open input file InputText.txt\n");
+		exit(1);
+	}
+	
+	int i=0, c, count=0;
+	while ((c = fgetc(ifp)) != EOF) {
+		if (c >= 65 && c <= 90){
+			found[(char)c - (int)'A']++;
+			count++;
+		} else if (c >= 97 && c <= 122) {
+			found[((char)c - (int)'a')+ALPH]++;
+			count++;
+		}
+	}
+	
+	for (int i=0; i < ALPH*2; i++){
+		found[i] = found[i] / count;
+	}
 }
 
 char rotate (char ch, int num) {
@@ -44,6 +63,9 @@ void decrypt (int key, char fname[]) {
 }
 
 int main () {
-	readFreq(letFreq, letterFreqFile);
+	readFreq (letFreq, letterFreqFile);
+	printf ("%f\n", letFreq[1]);
+	calcFreq (inputFreq, readFile);
+	printf ("%f\n", inputFreq[1]);
 	return 0;
 }
